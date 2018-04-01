571c46692c8f0fe496051101ace177360253f7b7
smallyunet <499503799@qq.com>
2018-04-01 15:18:32 +0800

    使用构造方法

--- CoreJava/day03/Lesson/Student.java
+++ CoreJava/day03/Lesson/Student.java
@@ -12,6 +12,21 @@ class Student {
     double height;
     String gender = "女";
 
+    public Student() {} // 构造方法
+    public void Student() {}  // 普通方法，因为有返回类型
+
+    public Student(String n, int a) { // 允许存在多个构造方法，参数必须不同
+      name = n;
+      age = a;
+    }
+
+    public Student(String n, int a, double h, String g) {
+      name = n;
+      age = a;
+      height = h;
+      gender = g;
+    }
+
     public void sayHello() {
       System.out.println("你好");
     }
@@ -33,5 +48,17 @@ class Student {
 
       String result2 = smallyu.bite1("我");
       System.out.println(result2);
+
+      Student user1 = new Student("user1", 25); // 传入初值
+      System.out.println(user1.name);
+      System.out.println(user1.age);
+      System.out.println(user1.height);
+      System.out.println(user1.gender);
+
+      Student user2 = new Student("user2", 30, 1.70, "男");
+      System.out.println(user2.name);
+      System.out.println(user2.age);
+      System.out.println(user2.height);
+      System.out.println(user2.gender);
     }
 }
---
+++ CoreJava/day03/Task/README.md
@@ -0,0 +1,7 @@
+#### 金山打字测速
+
+![images](images/4.1.1.png)
+
+![images](images/4.1.2.png)
+
+![images](images/4.1.3.png)
