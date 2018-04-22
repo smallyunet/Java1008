3559674b319e64cd753d73b7c5ad513ff2ea0931
smallyunet <499503799@qq.com>
2018-04-22 11:06:50 +0800

    day07 11:06

--- CoreJava/Day07/Lesson/IfDemo.java
+++ CoreJava/Day07/Lesson/IfDemo.java
@@ -1,7 +1,7 @@
 /*
  * ychs版权所有
  */
-package com.ychs168.corejava.lesson07;
+package com.ychs168.corejava;
 
 /**
  * if语句语法示例
@@ -9,7 +9,21 @@ package com.ychs168.corejava.lesson07;
  * @version 1.0 2018-04-22 
  */
 public class IfDemo {
-    public satic void main(String[] args) {
-        
+    public static void main(String[] args) {
+
+        // if (true) 
+        //     int age = 10;    // 报错
+
+        int age;
+        if (true)
+            age = 20; // 正确的
+
+        if (false);{ // 提前结束
+            System.out.println(123);
+            System.out.println(456);
+        }
+
+        if (true)
+            System.out.println("Hello");
     }
 }
--- CoreJava/Day07/Task/Task2.java
+++ CoreJava/Day07/Task/Task2.java
@@ -4,17 +4,28 @@
 
 package com.ychs168.corejava;
 
+import java.util.Scanner;
+
 /**
  *  控制台接收张浩的 Java 成绩，音乐成绩。
  *  当张浩Java成绩大于98分，而且音乐成绩大于80分，老师奖励他；
  *  或者Java成绩等于100分，音乐成绩大于70分，老师也可以奖励他
  */
 public class Task2 {
+
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("请输入java成绩：");
-        int javaScore = new java.util.Scanner(System.in).nextInt();
+        int javaScore = sc.nextInt();
         System.out.println("请输入music成绩：");
-        int musicScore = new java.util.Scanner(System.in).nextInt();
+        int musicScore = sc.nextInt();
+        new Task2().award(javaScore, musicScore);
+    }
+
+    /**
+     * 判断学生是否可以获得奖励
+     */
+    public void award(int javaScore, int musicScore) {
         if ((javaScore > 98 && musicScore > 80) || (javaScore > 100 && musicScore > 70)) {
             System.out.println("张浩可以获得奖励");
         } else {
